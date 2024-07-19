fc8fb40a2c06b27962d8438830368238400fb958
Ishita25-sys <ishu.252502@gmail.com>
2024-07-19 12:27:27 +0530

    Update Program.cs

--- officeproj/Program.cs
+++ officeproj/Program.cs
@@ -15,7 +15,7 @@ namespace officeproj
             msg.From = new MailAddress(fromMail);
             msg.Subject = "Birthday Wishes...";
             msg.To.Add(new MailAddress("ishita25.visitorz@gmail.com"));
-            msg.Body = "<html><body background='https://img.freepik.com/free-vector/flat-golden-circle-balloons-birthday-background_52683-34659.jpg?t=st=1721368644~exp=1721372244~hmac=6c9d09b06ba6db26f868b7e8ee646796bd28da7743d262b6af7f398c4aedff33&w=996'>Happy Birthday<br><img src='https://as1.ftcdn.net/v2/jpg/03/25/69/56/1000_F_325695691_662cr0MVqqJ6NA1dzgmtlMtHCoT6aguX.jpg'></body></html>";
+            msg.Body = "<html><body background='https://raw.githubusercontent.com/Ishita25-sys/birthday-post/main/birthdaypost.jpg'></body></html>";
             msg.IsBodyHtml = true;
 
             var smtpClient = new SmtpClient("smtp.gmail.com")
